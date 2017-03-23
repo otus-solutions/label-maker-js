7905a596ce03c0553bc363fbc51bb5bd0c2520fe
emanoelvianna <emanoel.vianna@outlook.com>
2017-03-23 15:20:13 -0300

    [RELEASE]squashing 'feature-otus-163' into 'dev'

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
             APP_ROOT_PATH + '**/*-module.js',
             APP_ROOT_PATH + '**/*.js', {
                 pattern: 'tests/unit/**/*-spec.js',
+                pattern: 'tests/unit/**/example.js',
                 included: true
             }
         ],
