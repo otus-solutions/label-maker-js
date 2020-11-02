d49dd5c855666ba162752ff461a06b2f620d60ab
erickpoleto <erick-poleto@hotmail.com>
2020-11-02 14:00:42 -0300

    LM-7 labels-size criadas

--- app/otusjs-label-builder/label-components/size-label/default-label/default-label-component.js
+++ app/otusjs-label-builder/label-components/size-label/default-label/default-label-component.js
@@ -38,6 +38,8 @@
             JsBarcode(barcodeContainer[0], self.baseInfo.recruitment_number, BARCODE_DEFAULT_SETTINGS);
           }else if(self.componentLabel == 'biomaterial') {
             JsBarcode(barcodeContainer[1], self.biomaterial.code, BARCODE_DEFAULT_SETTINGS)
+          }else if(self.componentLabel == 'unattached') {
+            JsBarcode(barcodeContainer[2], self.biomaterial.code, BARCODE_DEFAULT_SETTINGS)
           }
         }
       }
